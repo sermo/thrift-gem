c035bbd7b94cf24a4b80909b3877e4bbdf5ac923
Rusty Geldmacher <rgeldmacher@sermo.com>
2013-12-03 13:57:39 -0500

    Do not build native extension on OSX
    We get these runtime failures on OS X when we use the Thrift native
    extension, so rather than invest time in determining why it fails, we
    will just avoid building the native extension when installing on OS X.
    
    This will be fine for a long-term solution since the goal will be to
    remove Thrift altogether eventually.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,4 @@
-# 
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements. See the NOTICE file
 # distributed with this work for additional information
@@ -6,21 +6,32 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License. You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# 
+#
 
-require 'mkmf'
+if RUBY_PLATFORM =~ /darwin|mac os/i
+  # The following is adapted from:
+  # http://stackoverflow.com/questions/17406246/native-extensions-fallback-to-pure-ruby-if-not-supported-on-gem-install
+  # Create a dummy Makefile, to satisfy Gem::Installer#install
+  File.open("Makefile", "wb") do |f|
+    f.puts <<-MAKEFILE
+.PHONY: install
+install:
+\t@echo "Extensions not installed, falling back to pure Ruby version."
+  MAKEFILE
+  end
+else
+  require 'mkmf'
+  $CFLAGS = "-g -O2 -Wall -Werror"
+  have_func("strlcpy", "string.h")
+  create_makefile 'thrift_native'
+end
 
-$CFLAGS = "-g -O2 -Wall -Werror"
-
-have_func("strlcpy", "string.h")
-
-create_makefile 'thrift_native'
--- thrift.gemspec
+++ thrift.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{thrift}
-  s.version = "0.5.2.sermo"
+  s.version = "0.5.3.sermo"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 1.2.0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Kevin Ballard, Kevin Clark, Mark Slee"]
